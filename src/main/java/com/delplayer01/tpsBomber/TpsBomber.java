907228fbf8c52f05d5047eabe49571ef98c099be
delplayer01 <delplayer31@gmail.com>
2025-07-27 15:11:34 -0500

    Initial commit

---
+++ src/main/java/com/delplayer01/tpsBomber/TpsBomber.java
@@ -0,0 +1,51 @@
+package com.delplayer01.tpsBomber;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TpsBomber extends JavaPlugin {
+
+    private static TpsBomber instance;
+
+    public static TpsBomber getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        getCommand("maps").setExecutor(new MapsCommand());
+        getCommand("maps").setTabCompleter(new MapsCommand());
+
+        for (int i = 1; i <= 5; i++) {
+            File mapFolder = new File(getDataFolder(), "Map" + i);
+            if (!mapFolder.exists()) {
+                mapFolder.mkdirs();
+                getLogger().info("Creada carpeta: " + mapFolder.getPath());
+            }
+
+            File tpFile = new File(mapFolder, "tp.yml");
+            if (!tpFile.exists()) {
+                try {
+                    tpFile.createNewFile();
+
+
+                    YamlConfiguration config = new YamlConfiguration();
+                    for (int tp = 1; tp <= 50; tp++) {
+                        config.set("tp" + tp, "0,0,0");
+                    }
+                    config.save(tpFile);
+
+                    getLogger().info("Archivo tp.yml creado y rellenado en " + tpFile.getPath());
+                } catch (IOException e) {
+                    getLogger().warning("No se pudo crear o guardar tp.yml en " + mapFolder.getName() + ": " + e.getMessage());
+                }
+            }
+        }
+    }
+}
+
